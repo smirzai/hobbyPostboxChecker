572b970705a7c7b6c5efcb4aff1eb3c7b109f684
saeid Mirzaei <smirzaei@paloaltonetworks.com>
2019-06-14 21:54:43 +0200

    sync

--- src/post_box_checker/post_box_checker_ota/post_box_checker_ota.ino
+++ src/post_box_checker/post_box_checker_ota/post_box_checker_ota.ino
@@ -5,31 +5,18 @@
 
 #include "passwords.h"
 
-const int infraredLedPin = 16;
+
 const int infraredLedFreq = 38000;
 const int infraredLedChannel = 0;
 const int infraredLedResolution = 8;
 
-long counter = 0;
-long counter2 = 0;
-boolean toggle = 1;
-boolean toggle2 = 1;
+
 #define LED_BUILTIN 5
 #define SENSOR_IN_PIN 17
- 
+#define INFRARED_LED_PIN  16
 
-void setup() {
-  
-   // configure LED PWM functionalitites
-  ledcSetup(infraredLedChannel, infraredLedFreq, infraredLedResolution);
-   pinMode(SENSOR_IN_PIN, INPUT_PULLUP);
 
-  
-    
-   // attach the channel to the GPIO to be controlled
-  ledcAttachPin(infraredLedPin, infraredLedChannel);
-  ledcWrite(infraredLedChannel, 0);
-  
+void setup_ota() {
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
@@ -85,38 +72,58 @@ void setup() {
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  pinMode(LED_BUILTIN, OUTPUT);
-hw_timer_t * timer = NULL;
- timer = timerBegin(0, 80, true);
+  
+}
 
+void setup() {
+  setup_ota();
+  
+   // configure LED PWM functionalitites
+ // ledcSetup(infraredLedChannel, infraredLedFreq, infraredLedResolution);
+ //  pinMode(SENSOR_IN_PIN, INPUT_PULLUP);
+
+  
+    
+   // attach the channel to the GPIO to be controlled
+ // ledcAttachPin(infraredLedPin, infraredLedChannel);
+ // ledcWrite(infraredLedChannel, 0);
+  
+ 
+ // pinMode(LED_BUILTIN, OUTPUT);
+  
+  // hw_timer_t * timer = NULL;
+  //timer = timerBegin(0, 80, true);
+
+ 
+ // checkMail();
 
 }
 
-boolean result = 0;
+volatile boolean result = 0;
+
+int toggle = 0;
+void IRAM_ATTR onTimer() {
+  toggle = ! toggle;
+  digitalWrite(LED_BUILTIN, toggle);
+  ledcWrite(infraredLedChannel, 128 * toggle) ;
+ 
+}
+
+// this function checks if any of the infrared ways are blocked
+// returns 1 if there is mail, 0 otherwise
+int checkMail() {
+  hw_timer_t * timer = NULL;
+ // timer = timerBegin(0, 80, true);
+  //timerAttachInterrupt(timer, &onTimer, true);
+  // timerAlarmWrite(timer, 100000, true);
+  //timerAlarmEnable(timer);
+  
+}
+
 
 void loop() {
   ArduinoOTA.handle();
-   counter++;
-   counter2++;
-   
-   if (counter2 > 50000) {
-      counter2 = 0;
-      counter = 0;
-      
-      toggle2 = !toggle2;
-     
-       ledcWrite(infraredLedChannel, 128
-       );
-      
-   }
-   if (counter >= 200 && toggle2) {
-      counter = 0;
-      toggle2 = 0;
-      digitalWrite(LED_BUILTIN, digitalRead(SENSOR_IN_PIN));
-     
-      ledcWrite(infraredLedChannel, 0);
-      
-   } 
-  
+ 
+ 
   
 }
